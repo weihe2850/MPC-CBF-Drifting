64cfb507e93072674be5da1330c83d054d43a86f
weihe2850 <weihe2850@163.com>
2024-08-11 17:15:36 +0800

    加入响应延迟模块

--- vehicle_path_tracking_model.m
+++ vehicle_path_tracking_model.m
@@ -1,22 +1,27 @@
-function x_update =  vehicle_path_tracking_model(x,u,Ref_path,Ts)%%Ԥ��ģ��
-    X_r=Ref_path(1);
-    Y_r=Ref_path(2);
-    phi_r=Ref_path(3);
-    vx_r=Ref_path(4);
-    vy_r=Ref_path(5);
-    r_r=Ref_path(6);
-    %�켣���ģ��
-    A=[0 0 -vx_r*sin(phi_r)-vy_r*cos(phi_r);
-       0 0  vx_r*cos(phi_r)-vy_r*sin(phi_r);
-       0 0               0            ];   
-    B=[cos(phi_r) -sin(phi_r) 0;
-       sin(phi_r)  cos(phi_r) 0;
-        0             0       1];   
-    C=eye(3); 
-%     ksi=[X-X_r;Y-Y_r;phi-phi_r];
-%     u = [vx-vx_r;vy-vy_r;r-r_r];
-    x_dot=A * x +B * u;
-    x_update=x+x_dot*Ts;                                                  
+function x_update = vehicle_path_tracking_model(x, u, u_prev, Ref_path, Ts, tau)
+    % 提取参考轨迹
+    X_r = Ref_path(1);
+    Y_r = Ref_path(2);
+    phi_r = Ref_path(3);
+    vx_r = Ref_path(4);
+    vy_r = Ref_path(5);
+    r_r = Ref_path(6);
 
+    % 轨迹跟踪模型
+    A = [0 0 -vx_r*sin(phi_r)-vy_r*cos(phi_r);
+         0 0  vx_r*cos(phi_r)-vy_r*sin(phi_r);
+         0 0               0            ];   
+    B = [cos(phi_r) -sin(phi_r) 0;
+         sin(phi_r)  cos(phi_r) 0;
+         0             0       1];   
+    C = eye(3); 
+
+    % 控制信号延迟模型 (一阶滞后)
+    u_delayed = (tau) * u + (1 - tau) * u_prev;
+
+    % 计算状态导数
+    x_dot = A * x + B * u_delayed;
+
+    % 更新状态
+    x_update = x + x_dot * Ts;
 end
-
